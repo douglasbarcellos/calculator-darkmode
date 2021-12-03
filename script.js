adfb5c4fbfc44ccea842260c8ce2f06da11a0f60
Your Name <douglasbarcellosti@gmail.com>
2021-12-03 19:31:18 -0300

    Botão

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //Calculadora com função Dark
+const switchbtn = document.getElementById("switch");
+const display = document.getElementById("display");
+const input = document.querySelectorAll("input");
+let value = "";
+switchbtn.addEventListener("click", ()=>{
+    if(switchbtn.checked == true){
+        document.body.setAttribute("data-theme", "dark");
+    } else {
+        document.body.setAttribute("data-theme", "");
+    }
+});
