476222f1ab8cda2b23b58aacbbef1b6f93b2e4e6
Your Name <douglasbarcellosti@gmail.com>
2021-12-03 19:47:22 -0300

    Finished

--- script.js
+++ script.js
@@ -10,3 +10,30 @@ switchbtn.addEventListener("click", ()=>{
         document.body.setAttribute("data-theme", "");
     }
 });
+
+input.forEach((e)=>{
+    e.addEventListener("click", (event)=>{
+        if(event.target.value == "="){
+            if(value.length != 0){
+                let newval = eval(value);
+                value = newval;
+                display.value = value;
+            }
+        } else if(event.target.value == 'C'){
+            value = "";
+            display.value = value;
+        } else if(event.target.value == "switch"){
+        } else{
+            value += event.target.value;
+            display.value = value;
+        }
+        if(!event.target.classList.contains("switch")){
+            event.target.classList.add("active");
+            setTimeout(()=>{
+                event.target.classList.remove("active");
+            },400);
+        }
+    }
+    )
+}
+)
